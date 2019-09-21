f11921bbffa2baa82f5cdf6dbece3a11756c1337
Bulgakoff <hlbu@yandex.ru>
2019-09-21 09:04:42 +0300

    добавил в ветку новые файлы

---
+++ TestDay.java
@@ -0,0 +1,32 @@
+package AlishevEnum48.Days;
+
+public class TestDay {
+    public static void main(String[] args) {
+        DayOfWeek weekDays = DayOfWeek.Saturday;
+        System.out.print("Случился  " + weekDays.getTrans());
+        System.out.println(" и осталось работать " + weekDays.getTimeLeft() + " часов. ");
+//        switch (weekDays) {
+//            case Monday:
+//                System.out.println(" timeLeft :" + 40 / 1 + "  hours");
+//                break;
+//            case Thursday:
+//                System.out.println("time left :" + 40 / 2 + "  hours");
+//                break;
+//            case Wednesday:
+//                System.out.println("time left :" + 40 / 3 + "  hours");
+//                break;
+//            case Tuesday:
+//                System.out.println("time left :" + 40 / 4 + "  hours");
+//                break;
+//            case Friday:
+//                System.out.println("time left :" + 40 / 5 + "  hours");
+//                break;
+//            case Saturday:
+//                System.out.println("time left :" + 40 / 6 + "  hours");
+//                break;
+//        }
+//
+//        //System.out.println(getWorkingHours(DayOfWeek.Monday));
+
+    }
+}
